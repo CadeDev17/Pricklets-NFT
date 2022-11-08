b18bbddc911f0e63efed4494bc918efc057a2894
Decryptr22 <decryptr22@gmail.com>
2022-11-08 18:31:37 -0500

    Edit Config

--- dist/app.js
+++ dist/app.js
@@ -8,11 +8,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const web_sdk_1 = __importDefault(require("@proton/web-sdk"));
+const web_sdk_1 = require("@proton/web-sdk");
 let activeLink;
 let activeSession;
 const account = 'decryptr';
